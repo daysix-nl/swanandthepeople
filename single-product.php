98156b27f1feaf54a8c2da592b6ee9a37fd48d55
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-05-16 15:29:45 +0200

    wip

--- single-product.php
+++ single-product.php
@@ -182,6 +182,36 @@ if ( post_password_required() ) {
                     <?php
                     }
                     ?>
+               
+                
+                <div class="grid gap-[12px] mt-[40px]">
+                    <div class="flex items-center">
+                        <div class="w-[35px]">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="17.124" height="12.329" viewBox="0 0 17.124 12.329">
+                                <path id="shipping-truck-svgrepo-com" d="M15.329,6H3V16.274H4.394a2.4,2.4,0,0,0,4.746,0h4.843a2.4,2.4,0,0,0,4.746,0h1.394V11.881L16.983,8.74H15.329Zm0,4.11v3.655A2.4,2.4,0,0,1,18.523,14.9h.231V12.448L16.415,10.11ZM13.959,14.9V7.37H4.37V14.9H4.6a2.4,2.4,0,0,1,4.333,0Zm2.4,2.055a1.027,1.027,0,1,1,1.027-1.027A1.027,1.027,0,0,1,16.357,16.959ZM7.795,15.932A1.027,1.027,0,1,1,6.767,14.9,1.027,1.027,0,0,1,7.795,15.932Z" transform="translate(-3 -6)" fill="#5D7365" fill-rule="evenodd"/>
+                            </svg>
+                        </div>
+                        <p class="text-15 leading-15 text-[#5D7365]">Gratis retourneren in NL</p>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="w-[35px]">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="15.327" height="13.554" viewBox="0 0 15.327 13.554">
+                                <path id="love" d="M10.47,4A4.264,4.264,0,0,0,7.363,5.348,4.257,4.257,0,0,0,0,8.257c0,4.238,6.836,8.4,7.133,8.562a.46.46,0,0,0,.474,0c.283-.163,7.119-4.324,7.119-8.562A4.261,4.261,0,0,0,10.47,4ZM7.363,15.882C6.174,15.123.92,11.575.92,8.257A3.336,3.336,0,0,1,6.986,6.34a.46.46,0,0,0,.752,0,3.336,3.336,0,0,1,6.068,1.917C13.806,11.573,8.553,15.121,7.363,15.882Z" transform="translate(0.3 -3.64)" fill="#5D7365" stroke="#5D7365" stroke-width="0.6"/>
+                            </svg>
+                        </div>
+                        <p class="text-15 leading-15 text-[#5D7365]">Voor, tijdens en na je zwangerschap</p>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="w-[35px]">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="15.274" height="16.007" viewBox="0 0 15.274 16.007">
+                                <path id="box2-svgrepo-com" d="M15.915,3.508,8.893.926,1.606,3.6v9.944l7.287,2.644,7.287-2.644V3.6l-.265-.1ZM8.623,15.516l-6.477-2.35V4.182l6.477,2.35Zm.27-9.461L2.653,3.791,8.893,1.5l6.234,2.292L8.893,6.055Zm6.747,7.111-6.477,2.35V6.532l6.477-2.34Z" transform="translate(-1.256 -0.553)" fill="#5D7365" stroke="#5D7365" stroke-width="0.7"/>
+                            </svg>
+
+                        </div>
+                        <p class="text-15 leading-15 text-[#5D7365]">Gratis verzending binnen NL/BE</p>
+                    </div>
+                </div>
+              
                 </div>
             </div>
         </div>
@@ -206,17 +236,88 @@ if ( post_password_required() ) {
                     </div>
                      <div id="tab3" class="tabcontent bg-[#f9f9f9] p-[25px] md:p-4 hidden">
                         <h2 class="font-normal text-22 leading-30 text-[#5D7365] mb-2">Beoordelingen</h2>
-                        <div class="font-normal text-15 leading-30 text-[#525252]">Er zijn momenteel geen beoordelingen beschikbaar.
-                           
-
-
-
-                        </div>
+                        <div class="font-normal text-15 leading-30 text-[#525252]">Er zijn momenteel geen beoordelingen beschikbaar.</div>
                     </div>
                 </div>
         </div>
     </section>
+    <?php if (get_field('testimonials')): ?>   
+    <!-- TESTIMONIALS -->
+    <section class="bg-white padding-bottom-true">
+        <div class="w-[360px] md:w-[600px] lg:w-[800px] mx-auto pb-[25px]">
+            <h2 class="text-36 leading-36 text-[#5D7365] font-crimson font-normal text-center"><?php echo get_field('titel');?></h2>
+            <?php if (get_field('subtitel')): ?> 
+                <h3 class="text-22 leading-22 text-[#5D7365] font-crimson font-normal mt-[15px] text-center"><?php echo get_field('subtitel');?></h3>
+            <?php endif; ?>
+        </div>
+        <div class="w-[360px] md:w-[745px] lg:w-[800px] mx-auto overflow-hidden">
+        <div class="swiper mySwiperTestimonials">
+                <div class="swiper-wrapper pb-[60px]">
+                    <?php
+                    if( have_rows('testimonials') ):
+                        while( have_rows('testimonials') ) : the_row(); ?>
+                        <?php
+                        $image = get_sub_field('afbeelding');
+                        $image_url = isset($image['url']) ? esc_url($image['url']) : '';
+                        $image_alt = isset($image['alt']) ? esc_attr($image['alt']) : '';
+                        ?>
+                        <div class="swiper-slide h-[400px] flex items-start md:items-center relative w-full bg-white">
+                            <div class="w-[510px] h-[370px] md:h-[270px] bg-[#f9f9f9] x-[1] md:z-[2] testimonial-blur rounded-[20px] p-[30px]">
+                                <div class="flex justify-between flex-col h-full">
+                                    <div class="grid">
+                                        <p class="text-40 leading-22 text-[#525252] font-crimson font-bold">"</p>
+                                        <p class="text-14 leading-25 text-[#525252]"><?php echo get_sub_field('quote');?></p>
+                                    </div>
+                                    <div class="grid mr-[110px] md:mr-[unset]">
+                                        <h3 class="text-22 leading-22 text-[#5D7365] font-crimson font-normal"><?php echo get_sub_field('naam');?></h3>
+                                        <?php if (get_sub_field('product')): ?>   
+                                        <?php
+                                            $current_product_id = get_sub_field('product'); // ID van het geselecteerde bericht
+                                            $product_post = get_post($current_product_id); // Het bericht ophalen
 
+                                            if ($product_post) { // Controleer of het bericht bestaat
+                                                $product_title = $product_post->post_title; // Titel van het bericht
+                                                $product_link = get_permalink($current_product_id); // Link naar het bericht
+                                            ?>
+                                                <a href="<?php echo esc_url($product_link); ?>" class="text-14 leading-25 text-[#525252]"><?php echo esc_html($product_title); ?></a>
+                                            <?php
+                                            }
+                                        ?>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="absolute md:top-0 right-0 bottom-0 w-[100px] md:w-[320px] bg-black h-[100px] md:h-full z-[2] md:z-[1] overflow-hidden rounded-[20px]">
+                                <img src="<?php echo $image_url; ?>" alt="<?php echo $image_alt; ?>" class="h-full min-h-full min-w-full object-cover object-center">
+                            </div>
+                        </div>
+
+                        <?php
+                        endwhile;
+                    else :
+                    endif;
+                    ?>
+                </div>
+                <div class="slider-2-next h-[40px] w-[40px] rounded-full bg-white absolute bottom-[1px] left-[calc(50%+10px)] z-50 shadow lg:hover:bg-[#f8f8f8] duration-300 flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="9.264" height="12.657" viewBox="0 0 9.264 12.657">
+                        <g id="Group_82" data-name="Group 82" transform="translate(1.697 1.697)">
+                            <line id="Line_3" data-name="Line 3" x2="5.359" y2="2.436" transform="matrix(0.94, 0.342, -0.342, 0.94, 0.833, 0)" fill="none" stroke="#5D7365" stroke-linecap="round" stroke-width="1"/>
+                            <line id="Line_4" data-name="Line 4" x2="5.359" y2="2.436" transform="matrix(-0.342, 0.94, -0.94, -0.342, 4.921, 4.227)" fill="none" stroke="#5D7365" stroke-linecap="round" stroke-width="1"/>
+                        </g>
+                    </svg>
+                </div>
+                <div class="slider-2-prev h-[40px] w-[40px] rounded-full bg-white absolute bottom-[1px] right-[calc(50%+10px)] z-50 shadow lg:hover:bg-[#f8f8f8] duration-300 flex items-center justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="9.264" height="12.657" viewBox="0 0 9.264 12.657" class="rotate-[180deg]">
+                        <g id="Group_82" data-name="Group 82" transform="translate(1.697 1.697)">
+                            <line id="Line_3" data-name="Line 3" x2="5.359" y2="2.436" transform="matrix(0.94, 0.342, -0.342, 0.94, 0.833, 0)" fill="none" stroke="#5D7365" stroke-linecap="round" stroke-width="1"/>
+                            <line id="Line_4" data-name="Line 4" x2="5.359" y2="2.436" transform="matrix(-0.342, 0.94, -0.94, -0.342, 4.921, 4.227)" fill="none" stroke="#5D7365" stroke-linecap="round" stroke-width="1"/>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </section>
+    <?php endif; ?>
 
 
 </main>
