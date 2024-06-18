628b7fbbf11099e2464823a16c50acd95f7be561
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-06-18 15:11:56 +0200

    wip

--- header.php
+++ header.php
@@ -10,6 +10,14 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P2LGQRWX');</script>
+<!-- End Google Tag Manager -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
@@ -22,7 +30,11 @@
     <?php wp_head(); ?>
   
 </head>
-<body <?php body_class( 'page-block menu-non-active' ); ?>>
+<body <?php body_class( 'page-block menu-non-active' ); ?> 
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P2LGQRWX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <header class="fixed top-0 left-0 right-0 z-[9999]">
     <div class="h-[35px] w-full bg-[#5D7365] hidden lg:block">
         <div class="h-[35px] lg:flex items-center justify-between max-w-[900px] mx-auto px-4 hidden opacity-80">
